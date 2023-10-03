e17d0794c9cfed7ee430de8f7af7b3692c89151a
Igor Kuznetsov <igor.kuznetsov@bindinc.nl>
2023-10-03 12:26:58 +0000

    Deployment to cloud run

--- Dockerfile.app
+++ Dockerfile.app
@@ -1,6 +1,5 @@
 # Use the official Python image as the base image
 FROM python:3.10-slim
-EXPOSE 8080
 
 # Set the working directory in Docker
 WORKDIR /app
@@ -18,6 +17,10 @@ RUN poetry config virtualenvs.create false \
 # Copy the rest of the application
 COPY chat_app .
 
-ENTRYPOINT ["streamlit", "run", "app.py", "--server.port=8080", "--server.address=0.0.0.0"]
+EXPOSE 8501
+
+HEALTHCHECK CMD curl --fail http://localhost:8501/_stcore/health
+
+ENTRYPOINT ["streamlit", "run", "app.py", "--server.port=8501", "--server.address=0.0.0.0"]
 # Command to run the application using gunicorn
 #CMD exec gunicorn --bind :$PORT --workers 1 --threads 8 --timeout 0 app:app
--- build_app.sh
+++ build_app.sh
@@ -3,8 +3,13 @@ REGION=europe-west1
 APP_NAME=binge-qa-app
 docker buildx build . --platform linux/amd64 --file Dockerfile.app --tag $APP_NAME
 # run locally
-# docker run -it --rm  --env-file ".env" -p 5000 $APP_NAME
+# docker run -it --rm  --env-file ".env" -p 8501:8501 $APP_NAME
 docker tag $APP_NAME gcr.io/$PROJECT_ID/$APP_NAME
 docker push gcr.io/$PROJECT_ID/$APP_NAME
 gcloud config set project $PROJECT_ID
-gcloud run deploy $APP_NAME --image gcr.io/$PROJECT_ID/$APP_NAME --platform managed --region $REGION --allow-unauthenticated --timeout=120 --set-env-vars "SEARCH_BASE_URL=https://binge-qa-api-iobao7k2yq-ew.a.run.app"
+gcloud run deploy $APP_NAME --image gcr.io/$PROJECT_ID/$APP_NAME --platform managed --region $REGION \
+    --allow-unauthenticated --timeout=120 --port=8501 \
+    --set-env-vars "SEARCH_BASE_URL=https://binge-qa-api-iobao7k2yq-ew.a.run.app"
+
+# the variable SEARCH_API_KEY has to be set manually in the running service
+# TODO use googlecloud secrets
